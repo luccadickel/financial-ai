6668604f766a18eadc9c33f76fdc66ed1aec52d7
luccadickel <luccadickel@hotmail.com>
2026-01-28 13:48:31 -0300

    Primeiro commit

---
+++ api/src/test/java/com/FinancialAI/FinancialAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinancialAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
